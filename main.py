718e16ef44fbda0a5e2759e83f255c73d222e281
bailichangchuan <80666730+bailichangchuan@users.noreply.github.com>
2025-11-14 02:16:31 +0000

    ADD pymakr config and boot file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-if __name__ == "__main__":
-    print("start smartClock")
-    print("initializing modules...")
+import pycom
+import time
+
+pycom.heartbeat(False)
+
+while True:
+    pycom.rgbled(0xFF0000)  # Red
+    time.sleep(1)
+    pycom.rgbled(0x00FF00)  # Green
+    time.sleep(1)
+    pycom.rgbled(0x0000FF)  # Blue
+    time.sleep(1)
