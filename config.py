2eddedb24fef37e857dfb422918424f558347bd5
bailichangchuan <80666730+bailichangchuan@users.noreply.github.com>
2025-11-15 13:13:46 +0000

    新增配置文件，所有配置信息在config.py进行

---
+++ config.py
@@ -0,0 +1,12 @@
+WIFI_NET_NAME: NO
+WIFI_PASSWORD: 1234567890
+API_KEY: ABCDEFGHIJ
+WIFI_SSID = "l888"       # 替换为你的WiFi SSID
+
+WIFI_PWD = "12345678"        # 替换为你的WiFi密码
+SENIVERSE_KEY = "xxxxxx"  # 替换为你的知心天气API Key
+LOCATION = "101320101"  # 香港固定城市ID
+# 知心天气API的域名和路径
+API_DOMAIN = "api.seniverse.com"
+API_PATH = f"/v3/weather/now.json?key={SENIVERSE_KEY}&location={LOCATION}&language=zh-Hans&unit=c"
+REFRESH_INTERVAL = 10  # 每10秒刷新（无需修改）
