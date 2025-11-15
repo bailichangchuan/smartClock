e0d287932bab2bd4ddfddf9f2908b66ac96c91e2
你的用户名 <lyl021016@163.com>
2025-11-16 01:05:52 +0800

    fix: 替换知心天气API Key为占位符

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 WIFI_SSID = "l888"       # 替换为你的WiFi SSID
 
 WIFI_PWD = "12345678"        # 替换为你的WiFi密码
-SENIVERSE_KEY = "S-3j5IWHEJFx2GkZv"  # 替换为你的知心天气API Key
+SENIVERSE_KEY = "xxxxxx"  # 替换为你的知心天气API Key
 LOCATION = "101320101"  # 香港固定城市ID
 # 知心天气API的域名和路径
 API_DOMAIN = "api.seniverse.com"
